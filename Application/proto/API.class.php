d3ab3b3c8b86b5d41c15cb12336fbdc6b045293c
Youssef El Ouatiq <youssefElOuatiq@Youssefs-MacBook-Pro.local>
2016-06-09 17:45:58 +0200

    masterkey mithilfe der PBKDF2 und daten aus DB wieder bilden

--- Application/proto/API.class.php
+++ Application/proto/API.class.php
@@ -38,12 +38,12 @@ abstract class API
     protected $file = Null;
 
 
-
     /**
      * Constructor: __construct
      * Allow for CORS, assemble and pre-process the data
      */
-    public function __construct($request) {
+    public function __construct($request)
+    {
         header("Access-Control-Allow-Orgin: *");
         header("Access-Control-Allow-Methods: *");
         header("Content-Type: application/json");
@@ -65,7 +65,7 @@ abstract class API
             }
         }
 
-        switch($this->method) {
+        switch ($this->method) {
             case 'DELETE':
             case 'POST':
                 $this->request = $this->_cleanInputs($_POST);
@@ -83,38 +83,42 @@ abstract class API
         }
     }
 
-    public function processAPI() {
+    public function processAPI()
+    {
         if (method_exists($this, $this->endpoint)) {
             return $this->_response($this->{$this->endpoint}($this->args));
         }
         return $this->_response("No Endpoint: $this->endpoint", 404);
     }
 
-    private function _response($data, $status = 200) {
+    private function _response($data, $status = 200)
+    {
         header("HTTP/1.1 " . $status . " " . $this->_requestStatus($status));
         return json_encode($data);
     }
 
-    private function _cleanInputs($data) {
+    private function _cleanInputs($data)
+    {
         $clean_input = Array();
-            if (is_array($data)) {
-                foreach ($data as $k => $v) {
-                    $clean_input[$k] = $this->_cleanInputs($v);
-                }
-            } else {
+        if (is_array($data)) {
+            foreach ($data as $k => $v) {
+                $clean_input[$k] = $this->_cleanInputs($v);
+            }
+        } else {
             $clean_input = trim(strip_tags($data));
         }
         return $clean_input;
     }
 
-    private function _requestStatus($code) {
+    private function _requestStatus($code)
+    {
         $status = array(
             200 => 'OK',
             404 => 'Not Found',
             405 => 'Method Not Allowed',
             500 => 'Internal Server Error',
         );
-        return ($status[$code])?$status[$code]:$status[500];
+        return ($status[$code]) ? $status[$code] : $status[500];
     }
 
 }
--- Application/proto/api.php
+++ Application/proto/api.php
@@ -43,5 +43,3 @@ class MyAPI extends API
         }
     }
 }
-
->
